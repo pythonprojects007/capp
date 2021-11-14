a9c42d5c5fc0dc24501ba76b824182a20c5cf7cc
Vipin Mathew <pythonprojects007@gmail.com>
2021-11-14 11:29:09 +0530

    changes

--- user_app.py
+++ user_app.py
@@ -5,20 +5,32 @@ from google.cloud import storage
 from firebase import firebase
 import os,datetime
 from Crypto.PublicKey import RSA
-img_file_buffer = streamlit.file_uploader("Upload an image")
-if img_file_buffer is not None:
-    image = Image.open(img_file_buffer)
-    print(image)
-    image.save("upload.jpg")
-    streamlit.image(image)
-    input_image = numpy.array(image)
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="cartoonifier-521b7-f1585f752196.json"
-    # firebase = firebase.FirebaseApplication('https://plenary-ridge-832.firebaseio.com/')
-    firebase = firebase.FirebaseApplication('https://cartoonifier-521b7.firebaseio.com/')
-    client = storage.Client()
-    bucket = client.get_bucket("cartoonifier-521b7.appspot.com")
-    # bucket = client.get_bucket("plenary-ridge-832.appspot.com")
-    # imagePath = [os.path.join(self.path,f) for f in os.listdir(self.path)]
-    imageBlob = bucket.blob("uploaded_image")
-    print(imageBlob.generate_signed_url(datetime.timedelta(seconds=300), method='GET'))
-    imageBlob.upload_from_filename('upload.jpg')
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+
+
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.i = 0
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        return img
+
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+# img_file_buffer = streamlit.file_uploader("Upload an image")
+# if img_file_buffer is not None:
+#     image = Image.open(img_file_buffer)
+#     print(image)
+#     image.save("upload.jpg")
+#     streamlit.image(image)
+#     input_image = numpy.array(image)
+#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="cartoonifier-521b7-f1585f752196.json"
+#     # firebase = firebase.FirebaseApplication('https://plenary-ridge-832.firebaseio.com/')
+#     firebase = firebase.FirebaseApplication('https://cartoonifier-521b7.firebaseio.com/')
+#     client = storage.Client()
+#     bucket = client.get_bucket("cartoonifier-521b7.appspot.com")
+#     # bucket = client.get_bucket("plenary-ridge-832.appspot.com")
+#     # imagePath = [os.path.join(self.path,f) for f in os.listdir(self.path)]
+#     imageBlob = bucket.blob("uploaded_image")
+#     print(imageBlob.generate_signed_url(datetime.timedelta(seconds=300), method='GET'))
+#     imageBlob.upload_from_filename('upload.jpg')
